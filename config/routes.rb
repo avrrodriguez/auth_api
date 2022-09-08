2c6d1c6cf85a89c61faad29fe1c57fe52b87ac28
Abraham V Rodriguez <pqavrr835@gmail.com>
2022-09-08 19:28:58 -0400

    Adds a user model, with a create route and controller, creates two instances of user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # Users create route
+  post "/users", controller: "users", action: "create"
 end
