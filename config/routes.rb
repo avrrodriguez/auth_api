3ee49b8c2a00f205e5fe9c394b44830d78b8ce42
Abraham V Rodriguez <pqavrr835@gmail.com>
2022-09-08 20:21:26 -0400

    Adds sessions controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # Users create route
   post "/users", controller: "users", action: "create"
+
+  # Authentication create route
+  post "/sessions", controller: "sessions", action: "create"
 end
